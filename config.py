1a3176232a8bd31634c9241656717f6ecd4881f5
desh-89 <desh_89@mail.ru>
2022-02-21 19:48:10 +0500

    Add available currency

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ TOKEN = "5007186022:AAHUj5b04fDILbxE4z34vUWsHWNvJe9Tqjc"
 exchanges = {
     'доллар': 'USD',
     'евро': 'EUR',
+    'иена': 'JPY',
+    'фунт': 'GBP',
+    'юань': 'CNY',
     'рубль': 'RUB'
 }
